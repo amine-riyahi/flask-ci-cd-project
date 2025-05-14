992d7e9e4ff9f33a1bad3484cbb52be5efeea27e
amine-riyahi <0696043242riyahi@gmail.com>
2025-05-14 09:33:23 -0400

    Fix test_client call

--- test_app.py
+++ test_app.py
@@ -4,9 +4,10 @@ from app import app
 
 class FlaskTest(unittest.TestCase):
     def test_home(self):
-        tester = app.test_client(self)
+        tester = app.test_client()  # ❌ remove 'self' here
         response = tester.get('/')
         self.assertEqual(response.status_code, 200)
 
 if __name__ == '__main__':
     unittest.main()
+
